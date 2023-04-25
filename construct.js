cc4868c596b56123dbcfd2e7dbb56382be5430b8
Matt <mhgarry92@gmail.com>
2023-04-25 03:09:19 -0400

    added shape test that passes as well as commented on how application works further

--- construct.js
+++ construct.js
@@ -1,26 +1,30 @@
 /// where we initialize the app and create the logo
+// we need to import inquirer, fs, our prompts, and our createShape function to render the svg file
 const inquirer = require('inquirer');
-const fs = require('fs');
-const prompts = require('./lib/prompt');
-const createShape = require('./lib/createshape')
+const fs = require('fs/promises');
+const prompts = require('./lib/prompt.js');
+const createShape = require('./lib/createshape.js');
 
-
-// creates the svg file using the data from the prompts
-function makeLogo(response) {
-const svg = createShape(response);
-	fs.writeFile('testSVG.svg', svg, ()=> console.log('Your logo has been created!'));
-}
-// initialize the app
-function init() {
-	inquirer.prompt(prompts)
-	.then((response) => {
-      makeLogo(response);
-	})
-	.catch((err) => {
-    console.log(err);
+// creates the svg file using the data from the prompts using the createShape function to make a new svg file from the class data passed to the createShape function from the prompts.js file. We are then writing the svg file to the lib folder and naming it test.svg
+function makeLogo(res) {
+  const svg = createShape(res);
+  fs.writeFile('lib/test.svg', svg, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('SVG created!');
   });
 }
-
-
+// we are intializiting the app by calling the init function which takes in the responses from the prompts.js file. After that it resolves the promise to use the makeLogo function to create the svg
+function init() {
+  inquirer.prompt(prompts)
+    .then((response) => {
+      makeLogo(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 init();
--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,10 +1,10 @@
-const Triangle = require('../lib/shapes');
-
-describe('Triangle', () => {
-	test('renders a blue triangle', () => {
-		const shape = new Triangle();
-		shape.setColor('blue');
-		expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
-	});
+const Shape = require('../lib/shapes');
+// describing a shape that has a color, text, and text color properties. We are creating this new shape object using our shape class constructor. Because we are using the Shape class  constructor, we are testing the shapeColor, shapeText, and TextColor properties of the shape class. These tests verify that the color, text, and text color properties of the new shape object are correct.
+describe('Shape', () => {
+  test('creates a new shape object with color, text, and text color properties', () => {
+    const shape = new Shape('red', 'dog', 'white');
+    expect(shape.shapeColor).toBe('red');
+    expect(shape.shapeText).toBe('dog');
+    expect(shape.textColor).toBe('white');
+  });
 });
-//we're testing if we're going to render a blue triangle
